a7e5775886bf121fe3022a7f0b7aa301ae56408f
Sebastian Gomez <sebgomez395@gmail.com>
2025-11-06 21:15:19 -0500

    Primer commit de la aplicacion React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Homepage from './homepage/homepage.jsx';
+import'./App.css';
+
+function App(){
+  return(
+    <>
+      <Homepage />
+    </>
+  );
+}
+export default App;
---
+++ src/homepage/homepage.jsx
@@ -0,0 +1,38 @@
+import React,{useState, useEffect} from "react";
+const homepage=()=>{
+
+    const [counter, setCounter]=useState(0);
+
+    const[counterSignal, setCounterSignal]=useState(0);
+
+    useEffect(()=>{
+        const intervalId=setInterval(()=>{
+            setCounterSignal(current=>current+1);
+        },1000);
+
+        return ()=>clearInterval(intervalId);
+    },[])
+    const changeValue=(value)=>{
+        setCounter(counter+value);
+    };
+
+    const resertValue=(value)=>{
+        setCounter(value);
+        setCounterSignal(value);
+    };
+
+    return(
+        <div>
+            <h1>Home</h1>
+            <h2>Fundamentos</h2>
+
+            <h1>Contador (Variable Normal): {counter}</h1>
+            <h1>Contador (Signal Automatico): {counterSignal}</h1>
+
+            <button onClick={()=>changeValue(1)}>Incrementar</button>
+            <button onClick={()=>changeValue(-1)}>Decrementar</button>
+            <button onClick={()=>resertValue(0)}>Resetear</button>
+        </div>
+    );
+}
+export default homepage;
